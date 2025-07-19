7db9501cb8bf816cf8686cd8d305333b5ebe7b35
Jarvx200 <jarvx404@gmail.com>
2025-07-19 19:13:59 +0300

    Readded main

---
+++ server/src/main/java/personal/jarvx/Main.java
@@ -0,0 +1,15 @@
+package personal.jarvx;
+
+import fr.jcgay.notification.Icon;
+import fr.jcgay.notification.Notification;
+import fr.jcgay.notification.Notifier;
+import fr.jcgay.notification.SendNotification;
+
+import java.net.URL;
+import java.net.URLStreamHandler;
+
+public class Main {
+    public static void main(String[] args) {
+        Server s = Server.getInstance();
+    }
+}
